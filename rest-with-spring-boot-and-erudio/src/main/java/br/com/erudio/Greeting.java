bded3d5d13f08ef91d1f6e68495bee45e8a94e1f
Marcos Pozzobon <marcosklann13@gmail.com>
2023-12-05 11:19:35 -0300

    First steps in Java with Spring Boot 3

---
+++ rest-with-spring-boot-and-erudio/src/main/java/br/com/erudio/Greeting.java
@@ -0,0 +1,33 @@
+package br.com.erudio;
+
+public class Greeting {
+	
+	private long id;
+	
+	private String content;
+
+	public Greeting(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
